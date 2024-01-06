7d61aa6254f9c047a76d47a57ab32ec7a1843db5
flyinglimao <hi@limaois.me>
2024-01-07 02:49:57 +0900

    Add interaction test

--- app/Button.stories.tsx
+++ app/Button.stories.tsx
@@ -1,15 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { expect, fn, userEvent, within } from "@storybook/test";
 
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
   title: "Button",
   component: Button,
-  args: {},
-  argTypes: {},
 };
 
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    onClick: fn(),
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole("button"));
+    await expect(args.onClick).toHaveBeenLastCalledWith(1);
+  },
+};
--- app/Button.tsx
+++ app/Button.tsx
@@ -1,3 +1,15 @@
-export function Button() {
-  return <button>Click me</button>;
+import { useState } from "react";
+
+export function Button({ onClick }: { onClick: (cnt: number) => void }) {
+  const [count, setCount] = useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+        onClick(count + 1);
+      }}
+    >
+      {count}
+    </button>
+  );
 }
